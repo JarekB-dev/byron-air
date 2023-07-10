7cb7bd31af5d78dc596951aa942536a726c54fa8
Jarek Bonk <jerrybonq@gmail.com>
2023-07-10 06:17:42 +0000

    add description to spreadsheet initialization.

--- run.py
+++ run.py
@@ -15,6 +15,8 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
+
+# Access to 'byron-air' google spreadsheet
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
